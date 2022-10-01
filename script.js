63f68bb3cd7bcd148fde93722c62084b6027026d
yuhmanashi <yzxu28@gmail.com>
2022-10-01 07:51:27 -0700

    all features finished

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const order = [];
+let animation = false;
 
 window.onload = function(){
     const boxes = document.getElementsByClassName('box')
     for (let box of boxes){
         box.onclick = function(){
-            if (!(order.includes(box.id))){
-                order.push(box.id);
-                box.style.backgroundColor = 'red';
-                
-                if (order.length === 11){
-                    setTimeout(clearBoxes, 500);
+            if (!animation){
+                if (!(order.includes(box.id))){
+                    console.log(box.id);
+                    order.push(box.id);
+                    box.style.backgroundColor = 'red';
+                    
+                    if (order.length === 11){
+                        animation = true;
+                        setTimeout(clearBoxes, 500);
+                        setTimeout(() => {
+                            animation = false;
+                        }, 12 * 500)
+                    }
                 }
-            } 
+            }
         }
     }
 }
